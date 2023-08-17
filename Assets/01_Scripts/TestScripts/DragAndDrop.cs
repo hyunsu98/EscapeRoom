0932a16ee47983eb3c14e14152ab1ed888c77f37
hyunsu98 <108320515+hyunsu98@users.noreply.github.com>
2023-08-17 19:20:49 +0900

    8/17

--- Assets/01_Scripts/TestScripts/DragAndDrop.cs
+++ Assets/01_Scripts/TestScripts/DragAndDrop.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,11 +14,34 @@ public class DragAndDrop : MonoBehaviour
 
     private void OnMouseDown()
     {
-        mousePosition = Input.mousePosition - GetMousePos();        
+        mousePosition = Input.mousePosition - GetMousePos();
     }
 
     private void OnMouseDrag()
     {
-        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);        
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+    }
+
+    private void Update()
+    {
+        MoveEtcObject();
+    }
+
+    private void MoveEtcObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            // ray 가 닿은 곳이 땅이라면
+            if (hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            {
+                // 새로운 위치 설정 및 UI 변경
+                // -> UI 변경 시 자동으로 위치가 변경됨
+                Vector3 pos = hitInfo.point;
+                pos.y = 0f;
+            }
+        }
     }
 }
